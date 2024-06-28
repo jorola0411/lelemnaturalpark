7fd896ee27517c623644cadfc45ac0876c13a3a8
jorola0411 <jorola@my.bcit.ca>
2024-06-28 12:05:17 -0700

    donate and ticket

--- donate.js
+++ donate.js
@@ -1,24 +1,3 @@
-let currentSlide = 0;
-
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-const nextButton = document.querySelector('.next');
-const prevButton = document.querySelector('.prev');
-
-nextButton.addEventListener('click', () => {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    updateSlidePosition();
-});
-
-prevButton.addEventListener('click', () => {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    updateSlidePosition();
-});
-
-function updateSlidePosition() {
-    const slidesContainer = document.querySelector('.slides');
-    slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
 document.addEventListener("DOMContentLoaded", function() {
     const donateBtn = document.getElementById("donateBtn");
     const donateModal = document.getElementById("donateModal");
