fc417c3f4760e70e032d259132b1cb3133f3201b
jorola0411 <jorola@my.bcit.ca>
2025-04-04 10:59:01 -0700

    mobile responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -31,27 +31,27 @@ function updateSlidePosition() {
     const slidesContainer = document.querySelector('.slides');
     slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const donateBtn = document.getElementById("donateBtn");
     const donateModal = document.getElementById("donateModal");
     const closeBtn = document.getElementById("closeBtn");
     const donateForm = document.getElementById("donateForm");
 
-    donateBtn.addEventListener("click", function() {
+    donateBtn.addEventListener("click", function () {
         donateModal.style.display = "block";
     });
 
-    closeBtn.addEventListener("click", function() {
+    closeBtn.addEventListener("click", function () {
         donateModal.style.display = "none";
     });
 
-    window.addEventListener("click", function(event) {
+    window.addEventListener("click", function (event) {
         if (event.target == donateModal) {
             donateModal.style.display = "none";
         }
     });
 
-    donateForm.addEventListener("submit", function(event) {
+    donateForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const donationAmount = document.getElementById("donationAmount").value;
         alert("Thank you for your donation of $" + donationAmount + "!");
