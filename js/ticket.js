e07c880119e00d6ebf94b106573247e594c8c284
jorola0411 <jorola@my.bcit.ca>
2025-04-15 15:54:01 -0700

    Added product pages and styling, as well as optimizations

--- js/ticket.js
+++ js/ticket.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const childTickets = parseInt(childTicketInput.value) || 0;
         const seniorTickets = parseInt(seniorTicketInput.value) || 0;
 
-        const totalCost = (adultTickets * 25) + (childTickets * 15) + (seniorTickets * 2);
-        totalCostElement.textContent = totalCost;
+        const totalCost = (adultTickets * 25) + (childTickets * 15) + (seniorTickets * 20);
+        totalCostElement.textContent = `$${totalCost.toFixed(2)}`;
     }
 
     adultTicketInput.addEventListener("input", updateTotalCost);
